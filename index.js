2b6dbb5f0c83280c6558f1c3c2afb40e14fdd458
myiuksa <77750606+myiuksa@users.noreply.github.com>
2022-02-08 19:46:26 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const ModalWindow = {
+    init() {
+        document.body.addEventListener("click", e => {
+            if (e.target.classList.contains("modal__close")) {
+                this.closeModal(e.target);
+            }
+        });
+      
+      this.openModal();
+    },
+
+    getHtmlTemplate(modalOptions) {
+        return `
+            <div class="modal__overlay">
+                <div class="modal__window">
+                    <div class="modal__titlebar">
+                        <span class="modal__title">${modalOptions.title}</span>
+                        <button class="modal__close material-icons">close</button>
+                    </div>
+                    <div class="modal__content">
+                        ${modalOptions.content}
+                    </div>
+                </div>
+            </div>
+        `;
+    },
+
+    openModal(modalOptions = {}) {
+        modalOptions = Object.assign({
+            title: 'Welcome',
+            content: 'Just write your country name, the country you are trading with, the numerical value of what you are trading and then the item you are trading. Happy trading guys'
+        }, modalOptions);
+
+        const modalTemplate = this.getHtmlTemplate(modalOptions);
+        document.body.insertAdjacentHTML("afterbegin", modalTemplate);
+    },
+
+    closeModal(closeButton) {
+        const modalOverlay = closeButton.parentElement.parentElement.parentElement;
+        document.body.removeChild(modalOverlay);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => ModalWindow.init());
